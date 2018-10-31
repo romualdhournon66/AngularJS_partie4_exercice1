ac9966255ce984c17273b2fcf07bfa1cd6b1ebb8
romualdhournon66 <k2001@cegetel.net>
2018-10-31 06:35:06 +0100

    mon assets

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+//Déclaration de l'application demoApp
+var demoApp = angular.module('demoApp',[
+  // Dépendances du "module"
+  'todoList'
+]);
+//Déclaration du module todoList
+var todoList = angular.module('todoList',[]);
